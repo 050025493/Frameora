a606d76ab520b58583c9a8a5ef2d4f38d3fa4782
050025493 <kunwaradityasingh7@gmail.com>
2025-12-30 01:26:42 +0530

    top picks and explore

--- script.js
+++ script.js
@@ -19,3 +19,53 @@
     track2.style.transform = `translateX(-${index2 * 100}%)`;
   }, 4000);
 
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    // 1. text slider
+    // waits for image follow the images
+    const perfTextSwiper = new Swiper('.perfume-text-swiper', {
+        direction: 'vertical',
+        slidesPerView: 1,
+        loop: true,             
+        allowTouchMove: false,  // Disable manual swiping on text
+        speed: 1000,            // Smooth animation speed
+        effect: 'slide'
+    });
+
+    // 2. image swiper
+    const perfImageSwiper = new Swiper('.perfume-image-swiper', {
+        direction: 'horizontal',
+        slidesPerView: 'auto',
+        centeredSlides: true,
+        spaceBetween: 40,
+        loop: true,
+        speed: 1000,  // Matches text speed
+        grabCursor:true,          
+        
+        // autoplay
+        autoplay: {
+            delay: 1500,                
+            disableOnInteraction: false, // dot interacion
+            pauseOnMouseEnter: true     // when hovered it is stopped
+        },
+
+        pagination: {
+            el: '.perfume-pagination',
+            clickable: true,
+        }
+    });
+
+    // 3. images slider forces text to move with it
+   
+    perfImageSwiper.on('slideChange', function () {
+        const activeIndex = perfImageSwiper.realIndex;
+        perfTextSwiper.slideToLoop(activeIndex, 1000);
+    });
+});
+
+
+
+
+
